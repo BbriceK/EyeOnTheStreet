17257d495ac601efc36374dbf02dbb0d11b7b7d4
qz-erica <qian.zhang2020@outlook.com>
2025-07-27 14:52:31 +0800

    update hyperparameter value

--- code/model.py
+++ code/model.py
@@ -36,7 +36,7 @@ def load_labels_from_json(json_file: str) -> dict:
 
 
 class AsymmetricLoss(nn.Module):
-    def __init__(self, gamma_neg=2, gamma_pos=1, clip=0.01, eps=1e-8, disable_torch_grad_focal_loss=True):
+    def __init__(self, gamma_neg=0.5, gamma_pos=0.3, clip=0.01, eps=1e-8, disable_torch_grad_focal_loss=True):
         super(AsymmetricLoss, self).__init__()
 
         self.gamma_neg = gamma_neg
